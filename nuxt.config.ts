4c5e78ccda4fdfd255b0f09d0f3e0f6dde0fb57d
Zyko <hata.s520@gmail.com>
2024-12-03 16:48:45 +0800

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxthub/core'],
+  hub: {
+    blob: true,
+    kv: true,
+    database: true,
+    cache: true,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
